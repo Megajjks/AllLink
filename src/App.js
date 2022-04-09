d8ff9b91705c0385356de3da5769ae8b1ce8bad0
Jayro Salazar <megajjks@gmail.com>
2022-04-08 19:39:23 -0500

    Feat: prework schema

--- src/App.js
+++ src/App.js
@@ -6,16 +6,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Nayeli and Aldo welcome to start</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          rel="noopener noreferrer">
+          AllLink
         </a>
       </header>
     </div>
